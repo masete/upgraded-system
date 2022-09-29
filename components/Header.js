8f586ea6b9fe3786dd1a94ae6755540189a35def
masete <nicholasmasete72@gmail.com>
2022-09-29 22:58:00 +0300

    added instagram logo for mobile version
    instagram logo

--- components/Header.js
+++ components/Header.js
@@ -7,13 +7,21 @@ const Header = () => {
         
         <div className='flex justify-between max-w-6xl'>
             {/* left */}
-            <div className='relative hidden lg:inline-grid h-24 w-24'>
-            <Image
-            src='https://links.papareact.com/ocw' 
-            layout='fill'
-            objectFit='contain'
-             />
-        </div>
+            <div className='relative hidden lg:inline-grid h-24 w-24 cursor-pointer'>
+              <Image
+              src='https://links.papareact.com/ocw' 
+              layout='fill'
+              objectFit='contain'
+              />
+            </div>
+
+            <div className='relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer'>
+              <Image
+              src='https://links.papareact.com/jjm' 
+              layout='fill'
+              objectFit='contain'
+              />
+            </div>
 
         {/* middle */}
 
