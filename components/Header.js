f5a7432ad0b6db2e74c87cdef37cb4c2f9cfaa40
masete <nicholasmasete72@gmail.com>
2022-09-30 23:46:05 +0300

    added styling to the search field

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         
         <div className='flex justify-between max-w-6xl'>
             {/* left */}
-            <div className='relative hidden lg:inline-grid h-24 w-24 cursor-pointer'>
+            <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
               <Image
               src='https://links.papareact.com/ocw' 
               layout='fill'
@@ -18,7 +18,7 @@ const Header = () => {
               />
             </div>
 
-            <div className='relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer'>
+            <div className='relative lg:hidden w-10 flex-shrink-0 cursor-pointer'>
               <Image
               src='https://links.papareact.com/jjm' 
               layout='fill'
@@ -27,11 +27,13 @@ const Header = () => {
             </div>
 
         {/* middle search input field*/}
-        <div>
-          <div>
+        <div className='relative mt-1 p-3 rounded-md'>
+          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
             <SearchIcon className='h-5 w-5 text-gray-500'/>
           </div>
-          <input type='text' placeholder='Search'/>
+          <input type='text' 
+          className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black' placeholder='Search'
+          />
         </div>
 
         {/* Right */}
