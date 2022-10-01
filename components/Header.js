c1f4cfc784f774b811bd0124aaf5e2db94b49d09
masete <nicholasmasete72@gmail.com>
2022-10-01 22:28:06 +0300

    middle header done

--- components/Header.js
+++ components/Header.js
@@ -27,14 +27,18 @@ const Header = () => {
             </div>
 
         {/* middle search input field*/}
-        <div className='relative mt-1 p-3 rounded-md'>
-          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
-            <SearchIcon className='h-5 w-5 text-gray-500'/>
+
+        <div className='max-w-sm'>
+            <div className='relative mt-1 p-3 rounded-md'>
+               <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+                   <SearchIcon className='h-5 w-5 text-gray-500'/>
+               </div>
+               <input type='text' 
+               className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' placeholder='Search'
+               />
           </div>
-          <input type='text' 
-          className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' placeholder='Search'
-          />
         </div>
+        
 
         {/* Right */}
         </div>
