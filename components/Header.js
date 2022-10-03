b3aaae137a40361755fee464bed9af53f4e9b086
masete <nicholasmasete72@gmail.com>
2022-10-03 22:22:32 +0300

    added custom css

--- components/Header.js
+++ components/Header.js
@@ -44,9 +44,9 @@ const Header = () => {
         {/* Right */}
 
         <div className='flex items-center justify-end space-x-4'>
-          <HomeIcon className='h-10 w-10' />
-          <MenuIcon className='h-10 w-10 md:hidden cursor-pointer' />
-          <PaperAirplaneIcon className='h-10 w-10' />
+          <HomeIcon className='navBtn' />
+          <MenuIcon className='h-6 w-10 md:hidden cursor-pointer' />
+          <PaperAirplaneIcon className='navBtn' />
         </div>
         </div>
         
