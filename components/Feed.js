ee03225cc2a99b4febadf6dfc73de689886bde8c
masete <nicholasmasete72@gmail.com>
2022-10-04 23:32:54 +0300

    added grid css to partiton display area

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Stories } from './Stories'
 
 const Feed = () => {
   return (
-    <main>
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
       {/* section */}
           {/* stories */}
           {/* posts */}
@@ -11,7 +12,9 @@ const Feed = () => {
           {/* mini profile */}
           {/* suggestions */}
 
-      <section></section>
+      <section>
+        <Stories />
+      </section>
       <section></section>
 
     </main>
