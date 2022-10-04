d9f729f32b884bd6cae7ef0d84acdb784ecd3d5b
masete <nicholasmasete72@gmail.com>
2022-10-04 07:29:40 +0300

    fix: fixed importation errors

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
-import styles from '../styles/Home.module.css'
+import Header from '../components/Header';
+import styles from '../styles/Home.module.css';
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
