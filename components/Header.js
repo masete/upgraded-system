37c33f75cf0303cbf2b0c84a3605ca6454fb5865
masete <nicholasmasete72@gmail.com>
2022-10-04 07:11:23 +0300

    added animations on airflight icon

--- components/Header.js
+++ components/Header.js
@@ -48,6 +48,10 @@ const Header = () => {
           <MenuIcon className='h-6 w-10 md:hidden cursor-pointer' />
           <div className='relative'>
             <PaperAirplaneIcon className='navBtn rotate-45' />
+            <div 
+            className='absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
+              3
+            </div>
           </div>
           
           <PlusCircleIcon className='navBtn' />
