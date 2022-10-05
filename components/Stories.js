ab203579efe0c1369728746a166cdd2db0752596
masete <nicholasmasete72@gmail.com>
2022-10-05 13:13:04 +0300

    added fake data from fackerjs

--- components/Stories.js
+++ components/Stories.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
+import faker from 'faker';
 
 export const Stories = () => {
+
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+
+    console.log(suggestions)
+  }, [])
+  
   return (
     <div>
       Stories
