9f6fb4e7c74c54f55d6d9d8c54d646b5d6152f51
masete <nicholasmasete72@gmail.com>
2022-10-07 00:01:20 +0300

    styled profile suggestions

--- components/Story.js
+++ components/Story.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Story = ({img, username}) => {
   return (
     <div>
-        <img src={img} alt='profile'/>  
-        <p>{username}</p>
+        <img className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer' src={img} alt='profile'/>  
+        <p className='text-xs w-14 truncate'>{username}</p>
     </div>
   )
 }
