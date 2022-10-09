0a71998a43bcc0f07f88ec9d1498e0f780a552e1
masete <nicholasmasete72@gmail.com>
2022-10-09 16:50:55 +0300

    added tailwind scroll bar

--- components/Stories.js
+++ components/Stories.js
@@ -18,7 +18,7 @@ export const Stories = () => {
     <div className='flex space-x-2 p-6 bg-white mt-8 border border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
       {suggestions.map((profile) =>(
         <Story key={profile.id}
-               img={profile.avatars}
+               img={profile.avatar}
                username={profile.username} />
       ))}
       {/* stories */}
