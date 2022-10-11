1a739baf54248a3ddd37674f07b29b72b5826a19
masete <nicholasmasete72@gmail.com>
2022-10-11 16:03:42 +0300

    feat: added post

---
+++ components/Post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>Iam a Post</div>
+  )
+}
+
+export default Post
--- components/Posts.js
+++ components/Posts.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Post from './Post';
 
 const Posts = () => {
   return (
-    <div>Posts</div>
+    <div>
+      <Post />
+      {/* */}
+       {/* */}
+        {/* */}
+         {/* */}
+          {/* */}
+           {/* */}
+            {/* */}
+    </div>
   )
 }
 
